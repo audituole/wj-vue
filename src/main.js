ef2f1eeb0476ca8ceb6e488dabc7d5941e96790b
audituole <wxhluo@outlook.com>
2020-07-17 17:07:31 +0800

    2020年7月17日17:07:13

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
-// 设置反向代理，前端请求默认发送到 http://localhost:8081/api
+// eslint-disable-next-line no-unused-vars
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 设置反向代理，前端请求默认发送到 http://localhost:8443/api
 var axios = require('axios')
-axios.defaults.baseUrl = 'http://localhost:8081/api'
-
+axios.defaults.baseURL = 'http://localhost:8081/api'
+// 全局注册，之后可在其他组件中通过 this.$axios 发送数据
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import Router from 'vue-router'
 import AppIndex from '@/components/home/Appindex'
 import Login from '@/components/Login'
 
-
 Vue.use(Router)
 
 export default new Router({
